2cebdf5ab55b97ebd0411a8d09fb50d40eb3d3c4
driveall <driveall@mail.ru>
2020-11-30 21:09:01 +0200

    init

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File f = new File("aaa.txt");
+        if (!f.exists()) {
+            f.createNewFile();
+        }
+        try (BufferedReader r = new BufferedReader(new FileReader("aaa.txt"));
+             BufferedWriter w = new BufferedWriter(new FileWriter("c:\\install\\bbb.txt"))) {
+            String s;
+            while ((s = r.readLine()) != null) {
+                w.write(s);
+                w.newLine();
+            }
+        }
+
+
+    }
+}

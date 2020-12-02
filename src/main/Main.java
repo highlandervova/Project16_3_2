bcdbe57ad6fdcddd2269b47777d8741444011ed8
highlandervova <highlandervova@gmail.com>
2020-12-02 21:29:50 +0200

    Without last new line.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,15 +8,22 @@ public class Main {
         if (!f.exists()) {
             f.createNewFile();
         }
-        try (BufferedReader r = new BufferedReader(new FileReader("aaa.txt"));
+        long t1 = System.currentTimeMillis();
+        try (BufferedReader r = new BufferedReader(new FileReader(f));
              BufferedWriter w = new BufferedWriter(new FileWriter("c:\\install\\bbb.txt"))) {
             String s;
-            while ((s = r.readLine()) != null) {
-                w.write(s);
+            s = r.readLine();
+            if(s != null) {w.write(s);}
+            while ((s = r.readLine())!= null) {
                 w.newLine();
+                w.write(s);
             }
         }
 
+        long t2 = System.currentTimeMillis();
+        System.out.println("BufferedReader+Writer "+(t2-t1)+" ms");
+
+        t1 = System.currentTimeMillis();
         File f2 = new File("ccc.dll");
         try (InputStream is = new BufferedInputStream(new FileInputStream(f2));
              OutputStream os = new BufferedOutputStream(new FileOutputStream(new File("c:\\install\\ddd.txt")))) {
@@ -25,5 +32,7 @@ public class Main {
                 os.write(i);
             }
         }
+        t2 = System.currentTimeMillis();
+        System.out.println("Input+Otput+Stream " + (t2-t1)+" ms");
     }
 }
